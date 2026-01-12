c1fdd30d51886315cd114ad84149a565b74350af
Matt Pocock <mattpocockvoice@gmail.com>
2026-01-12 10:10:35 +0000

    Fixed

--- exercises/01-ai-sdk-basics/01.13-devtools-basics/explainer/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var run_local_dev_server_ts_1 = require("#shared/run-local-dev-server.ts");
-await (0, run_local_dev_server_ts_1.runLocalDevServer)({
-    root: import.meta.dirname,
-});
